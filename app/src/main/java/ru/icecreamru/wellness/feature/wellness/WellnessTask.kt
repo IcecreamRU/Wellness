63d81be2c41d49dfec1df00dc6b0da5787f8dc8b
Denis.Zateev <zateevdenis@gmail.com>
2024-06-09 14:27:43 +0300

    add states

---
+++ app/src/main/java/ru/icecreamru/wellness/feature/wellness/WellnessTask.kt
@@ -0,0 +1,13 @@
+package ru.icecreamru.wellness.feature.wellness
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
